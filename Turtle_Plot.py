74064ecdcfcf6ff37d83d0e0c5579fc345befc7f
Carlos Prieto <carlos2000.prieto@gmail.com>
2019-11-14 19:39:01 +0100

    Add Plot Function

---
+++ Turtle_Plot.py
@@ -0,0 +1,23 @@
+#Main contributor: Carlos
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def turtlePlot(turtleCommands):
+    plotPoints = np.zeros([int(turtleCommands.size/2),2])
+    direction = np.array([1,0])
+    for i in range (int(turtleCommands.size/2)):
+        length = turtleCommands[2*(i+1)-2]
+        ang = turtleCommands[2*(i+1)-1]
+        direction = np.matmul(np.array([[math.cos(ang),-math.sin(ang)],[math.sin(ang),math.cos(ang)]]),direction) #From https://docs.scipy.org/doc/numpy/reference/generated/numpy.matmul.html
+        plotPoints[i] = plotPoints[i-1] + length * direction
+    plotPoints = np.concatenate((np.array([[0,0]]),plotPoints),axis=0)
+    
+    plt.plot(plotPoints[:,0],plotPoints[:,1])
+    plt.xlim(0,10)
+    plt.ylim(0,10)
+    plt.show()    
+    return
+
+turtlePlot(np.array([2,math.pi/3,1,math.pi/3,1,-math.pi/3,1,-math.pi/3,1,-math.pi/3,1,-math.pi/3,1,math.pi/3,1,math.pi/3]))
