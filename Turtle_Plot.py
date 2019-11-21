1136b1ab202ba99aeca877005250166bcc2cfc6b
Carlos Prieto <carlos2000.prieto@gmail.com>
2019-11-21 16:21:50 +0100

    The fucking line works now

--- Turtle_Plot.py
+++ Turtle_Plot.py
@@ -5,13 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def turtlePlot(turtleCommands, pattern):
-    plotPoints = np.zeros([int(turtleCommands.size/2),2])
+    plotPoints = np.zeros([int(turtleCommands.size/2)+1,2])
     direction = np.array([1,0])
-    for i in range (int(turtleCommands.size/2)):
-        length = turtleCommands[2*(i+1)-2]
-        ang = turtleCommands[2*(i+1)-1]
+    plotPoints[0] = np.array([turtleCommands[0],0])
+    for i in range (np.size(plotPoints,axis=0)-1):
+        length = turtleCommands[2*i]
+        ang = turtleCommands[2*i+1]
         direction = np.matmul(np.array([[math.cos(ang),-math.sin(ang)],[math.sin(ang),math.cos(ang)]]),direction) #From https://docs.scipy.org/doc/numpy/reference/generated/numpy.matmul.html
-        plotPoints[i] = plotPoints[i-1] + length * direction
+        plotPoints[i+1] = plotPoints[i] + length * direction
     plotPoints = np.concatenate((np.array([[0,0]]),plotPoints),axis=0)
     
     plt.plot(plotPoints[:,0],plotPoints[:,1])
