95b0e796186b44992c325a3d43c1b6687cfd2a4b
Nicomancer12321 <57219792+Nicomancer12321@users.noreply.github.com>
2019-11-21 14:13:43 +0100

    fjieo

--- Turtle_Plot.py
+++ Turtle_Plot.py
@@ -7,17 +7,19 @@ import matplotlib.pyplot as plt
 def turtlePlot(turtleCommands, pattern):
     plotPoints = np.zeros([int(turtleCommands.size/2),2])
     direction = np.array([1,0])
-    pltoPoint[i] = np.array([0,0])
-    for i in range (int(turtleCommands.size/2)-1):
+    for i in range (int(turtleCommands.size/2)):
         length = turtleCommands[2*(i+1)-2]
         ang = turtleCommands[2*(i+1)-1]
         direction = np.matmul(np.array([[math.cos(ang),-math.sin(ang)],[math.sin(ang),math.cos(ang)]]),direction) #From https://docs.scipy.org/doc/numpy/reference/generated/numpy.matmul.html
-        plotPoints[i+1] = plotPoints[i] + length * direction
+        plotPoints[i] = plotPoints[i-1] + length * direction
     plotPoints = np.concatenate((np.array([[0,0]]),plotPoints),axis=0)
     
     plt.plot(plotPoints[:,0],plotPoints[:,1])
-    plt.xlim(np.min(plotPoints[:,0])-0.25,np.max(plotPoints[:,0])+0.25)
-    plt.ylim(np.min(plotPoints[:,1])-0.25,np.max(plotPoints[:,1])+0.25)
+    plt.xlim(0,1)
+    if pattern == "Koch curve":
+        plt.ylim(-0.225,0.525)
+    if pattern == "Sierpinski triangle":
+        plt.ylim(0,np.max(plotPoints[:,1]))
     plt.title(pattern) #Maybe also have the number of iterations in the title
     plt.show()    
     return
